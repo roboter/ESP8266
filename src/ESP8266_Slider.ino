b0355f8f1f3ea30e58fd827239e722cbc520e2dd
Aleksei Prokopov <robby.roboter@gmail.com>
2018-11-11 19:30:44 +0200

    Trying a ArduinoMenu library

--- src/ESP8266_Slider.ino
+++ src/ESP8266_Slider.ino
@@ -1,234 +1,145 @@
-#include <spi.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
+#include <Arduino.h>
+#include <SPI.h>
+#include <menu.h>
+#include <menuIO/adafruitGfxOut.h>
+#include <menuIO/chainStream.h>
+#include <menuIO/keyIn.h>
 
-boolean backlight = false;
-int contrast = PWMRANGE;
+using namespace Menu;
 
-int menuitem = 1;
-int page = 1;
-//#define  BACKLIGHT  D0
-volatile boolean up = false;
-volatile boolean down = false;
-volatile boolean middle = false;
+#define BTN_SEL D2  // Select button
+#define BTN_UP D1   // Up
+#define BTN_DOWN D3 // Down
 
-int downButtonState = 0;
-int upButtonState = 0;
-int resetButtonState = 0;
-int selectButtonState = 0;
-int lastDownButtonState = 0;
-int lastSelectButtonState = 0;
-int lastUpButtonState = 0;
+Adafruit_PCD8544 gfx(D6, -1, -1); // 48x84
+//#define LEDPIN D4
 
-Adafruit_PCD8544 display = Adafruit_PCD8544(D6, -1, -1); //48�84
+result showEvent(eventMask e, navNode &nav, prompt &item) { return proceed; }
 
-void checkIfDownButtonIsPressed()
-{
-	if (downButtonState != lastDownButtonState)
-	{
-		if (downButtonState == 0)
-		{
-			down = true;
-		}
-		delay(50);
-	}
-	lastDownButtonState = downButtonState;
+int test = 55;
+int ledCtrl = LOW;
+
+result myLedOn() {
+  ledCtrl = HIGH;
+  return proceed;
+}
+result myLedOff() {
+  ledCtrl = LOW;
+  return proceed;
 }
 
-void checkIfUpButtonIsPressed()
-{
-	if (upButtonState != lastUpButtonState)
-	{
-		if (upButtonState == 0) {
-			up = true;
-		}
-		delay(50);
-	}
-	lastUpButtonState = upButtonState;
+result alert(menuOut &o, idleEvent e);
+result doAlert(eventMask e, prompt &item);
+
+TOGGLE(ledCtrl, setLed, "Led: ", doNothing, noEvent,
+       noStyle //,doExit,enterEvent,noStyle
+       ,
+       VALUE("On", HIGH, doNothing, noEvent),
+       VALUE("Off", LOW, doNothing, noEvent));
+
+char *constMEM hexDigit MEMMODE = "0123456789ABCDEF";
+char *constMEM hexNr[] MEMMODE = {"0", "x", hexDigit, hexDigit};
+char buf1[] = "0x11";
+
+MENU(mainMenu, "ESP8266 Timer", doNothing, noEvent, wrapStyle,
+     // FIELD(test, "Test", "%", 0, 100, 10, 1, doNothing, noEvent, wrapStyle),
+     SUBMENU(setLed), OP("LED On", myLedOn, enterEvent),
+     OP("LED Off", myLedOff, enterEvent),
+     // OP("Alert test", doAlert, enterEvent),
+     EDIT("Hex", buf1, hexNr, doNothing, noEvent, noStyle), EXIT("<Back"));
+
+// define menu colors --------------------------------------------------------
+//  {{disabled normal,disabled selected},{enabled normal,enabled selected,
+//  enabled editing}}
+// monochromatic color table
+const colorDef<uint16_t> colors[] MEMMODE = {
+    {{WHITE, BLACK}, {WHITE, BLACK, BLACK}}, // bgColor
+    {{BLACK, WHITE}, {BLACK, WHITE, WHITE}}, // fgColor
+    {{BLACK, WHITE}, {BLACK, WHITE, WHITE}}, // valColor
+    {{BLACK, WHITE}, {BLACK, WHITE, WHITE}}, // unitColor
+    {{BLACK, WHITE}, {WHITE, WHITE, WHITE}}, // cursorColor
+    {{BLACK, WHITE}, {WHITE, BLACK, BLACK}}, // titleColor
+};
+
+#define gfxWidth 84
+#define gfxHeight 48
+#define fontX 6
+// 5
+#define fontY 9
+#define MAX_DEPTH 2
+
+// encoderIn<encA, encB> encoder; // simple quad encoder driver
+// encoderInStream<encA, encB> encStream(encoder,4); // simple quad encoder fake
+// Stream
+
+// a keyboard with only one key as the encoder button
+// keyMap encBtn_map[] = {{-encBtn,defaultNavCodes[enterCmd].ch}}; // negative
+// pin numbers use internal
+// pull-up, this is on when low
+// keyIn<1> encButton(encBtn_map);      // 1 is the number of keys
+
+keyMap joystickBtn_map[] = {
+    {-BTN_SEL, defaultNavCodes[enterCmd].ch},
+    {-BTN_UP, defaultNavCodes[upCmd].ch},
+    {-BTN_DOWN, defaultNavCodes[downCmd].ch},
+};
+keyIn<3> joystickBtns(joystickBtn_map);
+
+MENU_INPUTS(in, &joystickBtns);
+
+#define MAX_DEPTH 2
+#define textScale 1
+MENU_OUTPUTS(out, MAX_DEPTH,
+             ADAGFX_OUT(gfx, colors, fontX, fontY,
+                        {0, 0, gfxWidth / fontX, gfxHeight / fontY}),
+             NONE);
+
+NAVROOT(nav, mainMenu, MAX_DEPTH, in, out);
+
+result alert(menuOut &o, idleEvent e) {
+  if (e == idling) {
+    o.setCursor(0, 0);
+    o.print(F("alert test"));
+    o.setCursor(0, 1);
+    o.print(F("press [select]"));
+    o.setCursor(0, 2);
+    o.print(F("to continue..."));
+  }
+  return proceed;
 }
 
-void checkIfSelectButtonIsPressed()
-{
-	if (selectButtonState != lastSelectButtonState)
-	{
-		if (selectButtonState == 0) {
-			middle = true;
-		}
-		delay(50);
-	}
-	lastSelectButtonState = selectButtonState;
-}
-void SetMenuColor(bool currentMenu)
-{
-	display.setTextColor(currentMenu ? WHITE : BLACK, currentMenu ? BLACK : WHITE);
+result doAlert(eventMask e, prompt &item) {
+  nav.idleOn(alert);
+  return proceed;
 }
 
-
-void drawMenu()
-{
-	if (page == 1)
-	{
-		display.setTextSize(1);
-		display.clearDisplay();
-		display.setTextColor(BLACK, WHITE);
-		display.setCursor(0, 0);
-		display.print("ESP8266 Timer");
-		display.drawFastHLine(0, 10, 83, BLACK);
-		display.setCursor(0, 15);
-
-		SetMenuColor(menuitem == 1);
-
-		display.print("Delay: ");
-		display.print(contrast);
-		display.setCursor(0, 25);
-
-		SetMenuColor(menuitem == 2);
-		display.print("Light: ");
-		display.print(backlight ? "ON" : "OFF");
-
-		display.display();
-
-		SetMenuColor(menuitem == 3);
-		display.setCursor(0, 35);
-		display.print("Start");
-		display.display();
-	}
-	else if (page == 2)
-	{
-		display.setTextSize(1);
-		display.clearDisplay();
-		display.setTextColor(BLACK, WHITE);
-		display.setCursor(15, 0);
-		display.print("DELAY");
-		display.drawFastHLine(0, 10, 83, BLACK);
-		display.setCursor(5, 15);
-		display.print("Value");
-		display.setTextSize(2);
-		display.setCursor(5, 25);
-		display.print(contrast);
-
-		display.setTextSize(2);
-		display.display();
-	}
-}
-void setContrast()
-{
-
-}
-void turnBacklightOn()
-{
-	contrast = 0;
-	setContrast();
-}
-
-void turnBacklightOff()
-{
-	contrast = PWMRANGE;
-	setContrast();
-}
-
-void resetDefaults()
-{
-	contrast = PWMRANGE;
-	setContrast();
-	backlight = false;
+// when menu is suspended
+result idle(menuOut &o, idleEvent e) {
+  o.setCursor(0, 0);
+  o.print(F("suspended..."));
+  o.setCursor(0, 1);
+  o.print(F("press [select]"));
+  o.setCursor(0, 2);
+  o.print(F("to continue"));
+  return proceed;
 }
 
 void setup() {
-
-	pinMode(D1, INPUT_PULLUP);
-	pinMode(D2, INPUT_PULLUP);
-	pinMode(D3, INPUT_PULLUP);
-	pinMode(D4, INPUT_PULLUP);
-
-	setContrast();
-	Serial.begin(9600);
-
-	display.begin();
-	display.setRotation(2);
-	display.clearDisplay();
-	display.display();
-	drawMenu();
+  joystickBtns.begin();
+  nav.idleTask = idle; // point a function to be used when menu is suspended
+  SPI.begin();
+  gfx.begin();
+  gfx.setRotation(2);
+  gfx.clearDisplay();
+  gfx.display(); // show splashscreen
 }
 
 void loop() {
-
-
-
-	downButtonState = digitalRead(D3);
-	selectButtonState = digitalRead(D2);
-	upButtonState = digitalRead(D1);
-	resetButtonState = digitalRead(D4);
-
-	checkIfDownButtonIsPressed();
-	checkIfUpButtonIsPressed();
-	checkIfSelectButtonIsPressed();
-
-	if (up && page == 1) {
-		up = false;
-		menuitem--;
-		if (menuitem == 0)
-		{
-			menuitem = 3;
-		}
-		drawMenu();
-	}
-	else if (up && page == 2) {
-		up = false;
-		contrast--;
-		if (contrast < 0) contrast = 0;
-		setContrast();
-		drawMenu();
-	}
-
-	if (down && page == 1) {
-		down = false;
-		menuitem++;
-		if (menuitem == 4)
-		{
-			menuitem = 1;
-		}
-		drawMenu();
-	}
-	else if (down && page == 2) {
-		down = false;
-		contrast++;
-		if (contrast > PWMRANGE) contrast = PWMRANGE;
-		setContrast();
-		drawMenu();
-	}
-	if (!resetButtonState) {
-		//    resetDefaults();
-		//     drawMenu();
-	}
-
-	if (middle) {
-		middle = false;
-
-		if (page == 1 && menuitem == 2)
-		{
-			if (backlight)
-			{
-				backlight = false;
-				turnBacklightOff();
-			}
-			else
-			{
-				backlight = true;
-				turnBacklightOn();
-			}
-		}
-
-		if (page == 1 && menuitem == 3)
-		{
-			resetDefaults();
-		}
-		else if (page == 1 && menuitem == 1) {
-			page = 2;
-		}
-		else if (page == 2) {
-			page = 1;
-		}
-		drawMenu();
-	}
+  nav.doInput();
+  if (nav.changed(0)) { // only draw if changed
+    nav.doOutput();
+    gfx.display();
+  }
 }
